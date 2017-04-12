4ea0736852a255e1c6f86259acb31f3fb850b5d7
Shea Knox <Shea Knox>
2017-04-11 22:13:14 -0500

    Current month populates.  Input hours display.  Total showing up as NaN

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -23,4 +23,50 @@ function logout () {
   });
 }
 
+var d = new Date();
+var month = new Array();
+month[0] = "January";
+month[1] = "February";
+month[2] = "March";
+month[3] = "April";
+month[4] = "May";
+month[5] = "June";
+month[6] = "July";
+month[7] = "August";
+month[8] = "September";
+month[9] = "October";
+month[10] = "November";
+month[11] = "December";
+var n = month[d.getMonth()];
 
+document.getElementById("currentMonth").innerHTML = n;
+document.getElementById("currentYear").innerHTML =  "This Year"
+
+var totalHours = 0;
+// Click function to add hours to the table on the page
+$("#submit").on('click', function(){
+  totalHours+=parseInt(inputHours);
+  var inputHours = $(".form-control").val();
+  $(".form-control").val("");
+  console.log(inputHours);
+  console.log(typeof(totalHours));
+  $("#tbody").append("<tr><td>"+inputHours+"</td><td>"+totalHours+"</td></tr>")
+
+});
+
+
+var d = new Date();
+var month = new Array();
+month[0] = "January";
+month[1] = "February";
+month[2] = "March";
+month[3] = "April";
+month[4] = "May";
+month[5] = "June";
+month[6] = "July";
+month[7] = "August";
+month[8] = "September";
+month[9] = "October";
+month[10] = "November";
+month[11] = "December";
+var n = month[d.getMonth()];
